a3cef4ab26979af68df2dab3320c7cd54d559f55
jantayreyes <jantayreyes@gmail.com>
2020-09-02 13:29:51 +0200

    Bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #index, new, create, show
+  resources :restaurants, only: [:index, :show, :new, :create] do
+  # restaurants/restaurant_id/reviews/new
+  resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Clearing database..."
+Restaurant.destroy_all
+
+
+puts "Creating restaurants"
+
+rest_1 = {name: "McChicken", address: "Calle Locura 15", category: "french" }
+rest_2 = {name: "Colombia Sabor", address: "Calle Reina 21", category: "chinese" }
+rest_3 = {name: "Durum Rice", address: "Calle Lopez de Vega 61", category: "french" }
+rest_4 = {name: "Pizzano", address: "Calle Roma 115", category: "italian" }
+rest_5 = {name: "Chinese Room", address: "Calle Pekin 64", category: "chinese" }
+
+[ rest_1, rest_2, rest_3, rest_4, rest_5 ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
